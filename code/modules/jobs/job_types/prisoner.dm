ss220-space/Skyrat-tg

Tweak: decrease prisoners amount

code/modules/jobs/job_types/prisoner.dm
@@ -3,7 +3,7 @@
 	description = "Keep yourself occupied in permabrig."
 	department_head = list("The Security Team")
 	faction = FACTION_STATION
-	total_positions = 12		// SKYRAT EDIT: Original value (0)
+	total_positions = 3		// SKYRAT EDIT: Original value (0)
 	spawn_positions = 2
 	supervisors = "the security team"
 	exp_granted_type = EXP_TYPE_CREW

Neironetys/skyrat1984test

code/modules/jobs/job_types/prisoner.dm
@@ -3,7 +3,7 @@
 	description = "Keep yourself occupied in permabrig."
 	department_head = list("The Security Team")
 	faction = FACTION_STATION
-	total_positions = 12		// NOVA EDIT: Original value (0)
+	total_positions = 3		// NOVA EDIT: Original value (0)
 	spawn_positions = 2
 	supervisors = "the security team"
 	exp_granted_type = EXP_TYPE_CREW